67aab625dd6a2f835dbacd96cf43b4fa77de573c
cupskydev <yusuf.wandana1@gmail.com>
2023-09-20 22:09:52 +0700

    feat: add skills in 'about' page + function script update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const defaultMode = "dark";
+
 const lightModeClass = {
   mode: "light",
   revertMode: "dark",
@@ -10,15 +12,19 @@ const darkModeClass = {
   iconSrc: "img/icon/sun.ico",
 };
 
-// set data when document loaded
+/** set data when document loaded */
 document.addEventListener("DOMContentLoaded", () => {
+  /** set default mode */
+  setMode(defaultMode);
+
+  /** set default content when document loaded */
   document.getElementById("content").innerHTML = getContent("__about");
 });
 
-// load navbar items
+/** load navbar items */
 document.querySelector(".__navbar").innerHTML = navbarItemsElements();
 
-// give function to .nav-links for changing the page
+/** give function to .nav-links for changing the page */
 var navbarLink = document.querySelectorAll(".nav-link");
 navbarLink.forEach((element) => {
   element.addEventListener("click", (e) => {
@@ -42,27 +48,11 @@ navbarLink.forEach((element) => {
   });
 });
 
-// set page mode (light or dark)
-var modeIcon = document.querySelector(".mode__icon");
-modeIcon.setAttribute("src", "img/icon/sun.ico");
-modeIcon.addEventListener("click", (e) => {
-  var elementTarget = e.target;
-  var body = document.querySelector("body");
-  var currentMode = elementTarget.getAttribute("mode");
-  var bodyClass, iconSrc, objectModeClass;
+/** set page mode (light or dark) */
+document.querySelector(".mode__icon").addEventListener("click", (e) => {
+  /** get currentMode */
+  var currentMode = e.target.getAttribute("mode");
 
-  // checking mode
-  if (currentMode === lightModeClass.mode) {
-    objectModeClass = lightModeClass;
-  } else {
-    objectModeClass = darkModeClass;
-  }
-
-  bodyClass = objectModeClass.mode;
-  revertMode = objectModeClass.revertMode;
-  iconSrc = objectModeClass.iconSrc;
-
-  body.setAttribute("class", bodyClass);
-  elementTarget.setAttribute("mode", revertMode);
-  elementTarget.setAttribute("src", iconSrc);
+  /** change mode. this function from functions.js */
+  setMode(currentMode);
 });
