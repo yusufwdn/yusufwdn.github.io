fbc96117188db87760ffb80a3b5fd40e08fcb3ad
Yusuf Wandana <53157399+yusufwdn@users.noreply.github.com>
2023-09-30 23:38:15 +0700

    feat: add cv file

--- src/parts/Home/Hero.js
+++ src/parts/Home/Hero.js
@@ -61,12 +61,13 @@ export default function Hero() {
             </a>
           </div>
         </div>
-        <button
+        <a
+          href={require("../../assets/files/CV_Yusuf_Wandana_-_Web_Developer.pdf")}
           className="bg-purple-500 px-4 py-2 rounded-lg text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-purple-700 duration-300"
-          type="button"
+          target="blank"
         >
           Download CV
-        </button>
+        </a>
         <button
           className="bg-purple-100 p-2 mt-20 rounded-full animate-bounce"
           type="button"
