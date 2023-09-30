99fafb700633de041cecfb8c46aaf63397371b60
Yusuf Wandana <53157399+yusufwdn@users.noreply.github.com>
2023-09-30 23:41:01 +0700

    fix: made some change on common text

--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -36,7 +36,7 @@ export default function Skill() {
   const skills = [
     {
       title: "Laravel / Lumen",
-      star: 9,
+      star: 8,
       description:
         "Dengan pengalaman bekerja lebih dari 2 tahun, saya telah mengerjakan berbagai proyek dengan studi kasus beragam. Tentunya saya telah memiliki pemahaman yang kuat tentang lingkungan dan kerangka kerja Laravel.",
       imageComponent: {
@@ -48,7 +48,7 @@ export default function Skill() {
     },
     {
       title: "Livewire",
-      star: 8,
+      star: 7,
       description:
         "Memiliki pemahaman yang cukup kuat dalam pengembangan web menggunakan Livewire. Karena saya sudah mengerjakan beberapa proyek dengan menggunakan Livewire sebagai komponen pendukung dari proyek yang berbasis Laravel.",
       imageComponent: {
@@ -60,7 +60,7 @@ export default function Skill() {
     },
     {
       title: "Bootstrap",
-      star: 9,
+      star: 8,
       description:
         "Saya cukup mahir dalam menggunakan Bootstrap. Karena selama tergabung dalam suatu proyek berbasis web, saya sering kali menggunakan Bootstrap sebagai kerangka kerja untuk styling dan layouting.",
       imageComponent: {
@@ -84,7 +84,7 @@ export default function Skill() {
     },
     {
       title: "Node Js",
-      star: 6,
+      star: 5,
       description:
         "Memiliki pemahaman dan pengetahuan yang cukup baik pada fundamental Node Js, saya dapat membuat layanan RESTful API beserta unit testing sederhana.",
       imageComponent: {
@@ -108,7 +108,7 @@ export default function Skill() {
     },
     {
       title: "Go",
-      star: 5,
+      star: 4,
       description:
         "Memiliki pemahaman dan pengetahuan dasar dalam lingkungan kerja Go. Saya telah memiliki kemampuan untuk membuat RESTful API sederhana dengan menggunakan Gin.",
       imageComponent: {
@@ -132,7 +132,7 @@ export default function Skill() {
     },
     {
       title: "Git VCS",
-      star: 8,
+      star: 7,
       description:
         "Memiliki pemahaman yang cukup baik tentang konsep serta penggunaan perintah Git, menyelesaikan konflik pada commit, serta melakukan kolaborasi dengan tim lain pada suatu proyek. ",
       imageComponent: {
@@ -147,22 +147,22 @@ export default function Skill() {
   return (
     <>
       <Header />
-      <div className="flex flex-col">
+      <div className="flex flex-col bg-slate-100">
         <section className="h-100 px-4 py-12 mb-10 md:pl-10 md:pr-2 pl-8 pr-5 mt-12">
           <h5 className="text-center text-3xl mb-16 font-medium">
             <span className="border-b-2 border-purple-500">
-              Personal Skills
+              Kemampuan Personal
             </span>
           </h5>
           <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6">
             {skills.map(function (item, key) {
               return (
                 <div
-                  className="flex flex-col relative justify-center border-2 border-purple-500 rounded-xl py-4 px-6 md:mx-4 mx-0 mb-8 bg-gray-100 box-shadow-5"
+                  className="flex flex-col relative justify-center border-2 border-purple-500 rounded-xl py-4 px-6 md:mx-4 mx-0 mb-8 bg-white box-shadow-5"
                   key={`skill.${key}`}
                 >
                   <img
-                    className="absolute border-2 border-purple-500 bg-gray-100 p-1 rounded-md -left-6 -top-6 w-16 box-shadow-5"
+                    className="absolute border-2 border-purple-500 bg-white p-1 rounded-md -left-6 -top-6 w-16 box-shadow-5"
                     width={item.imageComponent.width}
                     height={item.imageComponent.height}
                     src={item.imageComponent.src}
